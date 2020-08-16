8c10ace17004d370315cbf78863f6489ec6e329b
Jaime <jaime.bustos.espinozaa@gmail.com>
2020-08-15 21:48:07 -0400

    Header modified

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="text-white sm:flex">
+    <header className="text-white sm:flex select-none">
       <div className='flex items-center justify-between px-4 lg:pl-8 py-4'>
         <div>
-          <h1 className='font-quick text-2xl md:text-4xl'>
+          <h1 className='font-quick text-2xl md:text-4xl sm:opacity-75 sm:hover:opacity-100 duration-200 cursor-pointer'>
             <span className='text-primary-600'>Scale</span>Soft
           </h1>
         </div>
@@ -29,7 +29,7 @@ export default function Header() {
           </button>
         </div>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} px-2 pt-2 pb-4 sm:p-0 sm:ml-auto sm:pr-1 lg:pr-6 font-open sm:flex sm:items-center`}>
+      <div className={`${isOpen ? 'block' : 'hidden'} px-2 pb-4 sm:p-0 sm:ml-auto sm:pr-1 lg:pr-6 font-open sm:flex sm:items-center`}>
         <a
           href='#'
           className='block px-2 py-1 sm:px-4 lg:px-6 text-sm font-normal sm:opacity-75 sm:hover:opacity-100 duration-200'
@@ -56,7 +56,7 @@ export default function Header() {
         </a>
         <a
           href='#'
-          className='mt-1 sm:mt-0 block px-2 py-1 sm:px-4 lg:px-6 text-sm font-normal sm:opacity-75 sm:hover:opacity-100 duration-200'
+          className='mt-1 sm:border-none sm:mt-0 block px-2 py-1 sm:px-4 lg:px-6 text-sm font-normal sm:opacity-75 sm:hover:opacity-100 duration-200'
         >
           Contacto
         </a>
