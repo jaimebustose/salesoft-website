cc5da78746df291006d647e731ec6ac843ce0e25
Jaime <jaime.bustos.espinozaa@gmail.com>
2020-08-19 10:21:26 -0400

    Fixed carousel

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -5,7 +5,10 @@ import ExtraElements from './ExtraElements';
 
 export default function SkillsSection() {
   return (
-    <section className='ss-skills pt-4 flex flex-col h-10'>
+    <section
+      className='ss-skills my-4 flex flex-col h-10 overflow-hidden'
+      style={{ height: 'fit-content' }}
+    >
       <div className='ss-slider my-6 flex' style={{ width: 'max-content' }}>
         <DefaultElements />
         <ExtraElements />
