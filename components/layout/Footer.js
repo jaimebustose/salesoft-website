ad0f72eb093887f66128435a700de63f717af728
manfred <manfred.engler.95@gmail.com>
2020-08-20 18:50:25 -0400

    Footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FooterAnimation from '../../public/res/footer-background.svg';
+import FooterContent from '../FooterContent';
 
 export default function Footer() {
   return (
     <footer className='ss-footer'>
       <div className='ss-footer-animation'>
-        <FooterAnimation className='w-full h-auto'/>
+        <FooterAnimation className='w-full h-auto' />
+        <FooterContent />
       </div>
     </footer>
-  )
+  );
 }
